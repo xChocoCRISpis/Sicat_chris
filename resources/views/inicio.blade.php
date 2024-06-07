034a73f232f61dddb01f3ee5cd4bd2bb16fcbf85
ChocoCHRISpis <crisneeda@gmail.com>
2024-06-06 23:06:41 -0600

    sidebar en layout

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app')
+<<<<<<< HEAD
 
 @section('title', 'Inicio')
 
@@ -35,3 +36,48 @@
         </div>  
     </section>
 @endsection
+=======
+@section('title', 'Inicio')
+@section('content')
+    <section>
+        <!-- <div class="barra">
+            <h2>SICAT</h2>
+        </div> -->
+        <section>
+            <div class="container-form">
+                <div class="actividad">
+                    <h2>Actividades Extraescolares</h2>
+                    <div class="depor">
+                        <a href="#"> <h1>Actividades Deportivas</h1></a>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima eius, nostrum sunt fugiat ratione quis ducimus illum minus sapiente fugit laudantium nemo praesentium unde illo adipisci, temporibus, laboriosam voluptatibus excepturi. Modi atque placeat delectus sit necessitatibus sapiente! Possimus repellat expedita alias deleniti aut tenetur assumenda quos porro minima, error a?</p>
+                        <div class="slider-frame">
+                            <ul>
+                                <li><img src="img/dep_bk.jpg" alt=""></li>
+                                <li><img src="img/dep_fb.jpg" alt=""></li>
+                                <li><img src="img/dep_tk.jpg" alt=""></li>
+                                <li><img src="img/dep_vb.jpg" alt=""></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="cult">
+                        <a href="#"> <h1>Actividades Culturales</h1></a>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae odio atque corrupti odit ullam nostrum ducimus quidem minima alias est dicta culpa labore eum qui omnis veniam facere fugit dolores, quod quam dolorum voluptate. Voluptas consequuntur id delectus, dignissimos pariatur dolorum sit iusto obcaecati. Quas laudantium neque qui officia sit!</p>
+                        <div class="slider-frame">
+                            <ul>
+                                <li><img src="img/cul_bg.jpg" alt=""></li>
+                                <li><img src="img/cul_dan.jpg" alt=""></li>
+                                <li><img src="img/cul_dz.jpg" alt=""></li>
+                                <li><img src="" alt=""></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        </section>
+</section>
+@endsection
+
+    <script src="js/script.js"></script>
+</body>
+</html>
+>>>>>>> c4f856e (sidebar en layout)
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,31 +7,6 @@
     <link rel="stylesheet" href="{{ asset('css/inicio.css') }}">
     <link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
     <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
-    <style>
-        /* Estilos adicionales para la sidebar */
-        .menu_side {
-            position: fixed;
-            width: 250px;
-            height: 100%;
-            background: #333;
-            color: #fff;
-            transition: transform 0.3s ease-in-out;
-            transform: translateX(-250px);
-        }
-        .menu_side.active {
-            transform: translateX(0);
-        }
-        main {
-            margin-left: 0;
-            transition: margin-left 0.3s ease-in-out;
-        }
-        main.shifted {
-            margin-left: 250px;
-        }
-        .icon_menu {
-            cursor: pointer;
-        }
-    </style>
 </head>
 <body>
     <header>
@@ -47,47 +22,33 @@
             <h4>SICAT</h4>
         </div>
         <div class="options_menu">
-            <a href="{{ route('inicio') }}" class="selected">
-                <div class="option">
-                    <i class="fas fa-house" title="Inicio"></i>
-                    <h4>Inicio</h4>
-                </div>
+            <a href="{{ route('inicio') }}" class="option selected">
+                <i class="fas fa-house" title="Inicio"></i>
+                <h4>Inicio</h4>
             </a>
-            <a href="{{ route('VerAlumnos') }}">
-                <div class="option">
-                    <i class="fas fa-people-robbery" title="Actividades"></i>
-                    <h4>Actividades</h4>
-                </div>
+            <a href="{{ route('VerAlumnos') }}" class="option">
+                <i class="fas fa-people-robbery" title="Actividades"></i>
+                <h4>Actividades</h4>
             </a>
-            <a href="#">
-                <div class="option">
-                    <i class="fas fa-pen-to-square" title="Eventos"></i>
-                    <h4>Eventos</h4>
-                </div>
+            <a href="#" class="option">
+                <i class="fas fa-pen-to-square" title="Eventos"></i>
+                <h4>Eventos</h4>
             </a>
-            <a href="#">
-                <div class="option">
-                    <i class="fab fa-gitter" title="Indicadores"></i>
-                    <h4>Indicadores</h4>
-                </div>
+            <a href="#" class="option">
+                <i class="fab fa-gitter" title="Indicadores"></i>
+                <h4>Indicadores</h4>
             </a>
-            <a href="#">
-                <div class="option">
-                    <i class="fab fa-leanpub" title="Reportes"></i>
-                    <h4>Reportes</h4>
-                </div>
+            <a href="#" class="option">
+                <i class="fab fa-leanpub" title="Reportes"></i>
+                <h4>Reportes</h4>
             </a>
-            <a href="#">
-                <div class="option">
-                    <i class="fas fa-person-shelter" title="Perfil"></i>
-                    <h4>Perfil</h4>
-                </div>
+            <a href="#" class="option">
+                <i class="fas fa-person-shelter" title="Perfil"></i>
+                <h4>Perfil</h4>
             </a>
-            <a href="#">
-                <div class="option">
-                    <i class="fas fa-person-walking-arrow-right" title="Salir"></i>
-                    <h4>Salir</h4>
-                </div>
+            <a href="#" class="option">
+                <i class="fas fa-person-walking-arrow-right" title="Salir"></i>
+                <h4>Salir</h4>
             </a>
         </div>
     </div>
